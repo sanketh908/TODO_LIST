9f63071443f828578b126908b36d57135700946e
sanketh908 <sankeths908@gmail.com>
2026-02-03 16:08:35 +0530

    added some class and interfaces

--- src/main/java/com/Sanekth/todo/Controller/TaskController.java
+++ src/main/java/com/Sanekth/todo/Controller/TaskController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class TaskController {
         model.addAttribute("tasks",taskList);
         return "tasks";
     }
-    @GetMapping
+    @PutMapping
     public String creatTask(@RequestParam String taskname)
     {
         taskService.creatNewTask(taskname);
